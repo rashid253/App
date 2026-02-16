9574503494cf4dd864bd3e0f72f95280c98687e3
faiqtech <rashidncit@gmail.com>
2026-02-16 17:07:10 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'ramadan-app-v1';
+const CACHE_NAME = 'ramadan-1447-v1';
 const urlsToCache = [
     'index.html',
+    'card.html',
     'calendar.html',
-    'manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+    'manifest.json'
 ];
 
 self.addEventListener('install', event => {
@@ -17,19 +17,5 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
-    );
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
     );
 });
